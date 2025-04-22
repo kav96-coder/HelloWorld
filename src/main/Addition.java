05c4c0d88576db235f7afc0caa1cfb88ede2cc92
Kavya Bharadwaj <kavyabharadwaj96@gmail.com>
2025-04-22 12:08:02 +0530

    Second commit

---
+++ src/main/Addition.java
@@ -0,0 +1,9 @@
+package src.main;
+
+public class Addition {
+    public static void main(String[] args) {
+        int a = 5, b = 10;
+        int sum = a + b;
+        System.out.println("Sum: " + sum);
+    }
+}
